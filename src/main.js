217e7a3ad1288a3fd1dadb41c41cb64a63ba6b8f
Raju Khunt <rajukhunt15@gmail.com>
2024-04-18 21:56:03 +0530

    Added route and css

--- src/main.js
+++ src/main.js
@@ -1,103 +1,45 @@
 import { createApp } from "vue";
-import "./style.css";
-import AppRoot from "./App.vue";
+import { router } from "./router";
 import { createPinia } from "pinia";
-import router from "./router";
 
-import { definePreset } from "primevue/themes";
+import "./style.css";
+import App from "./App.vue";
+
 import PrimeVue from "primevue/config";
-import PrimeOne from "primevue/themes/primeone";
-import Aura from "primevue/themes/primeone/aura";
 
 import Card from "primevue/card";
-import Button from "primevue/button";
 import DataTable from "primevue/datatable";
 import Column from "primevue/column";
 import ColumnGroup from "primevue/columngroup";
 import Row from "primevue/row";
 import Dropdown from "primevue/dropdown";
-import InputText from "primevue/inputtext";
 import Divider from "primevue/divider";
 import Checkbox from "primevue/checkbox";
+import Dialog from "primevue/dialog";
+import Button from "primevue/button";
+import InputText from "primevue/inputtext";
+import MyPreset from "./mypreset";
 
-const Noir = definePreset(Aura, {
-  semantic: {
-    primary: {
-      50: "{zinc.50}",
-      100: "{zinc.100}",
-      200: "{zinc.200}",
-      300: "{zinc.300}",
-      400: "{zinc.400}",
-      500: "{zinc.500}",
-      600: "{zinc.600}",
-      700: "{zinc.700}",
-      800: "{zinc.800}",
-      900: "{zinc.900}",
-      950: "{zinc.950}",
-    },
-    colorScheme: {
-      light: {
-        primary: {
-          color: "{zinc.950}",
-          inverseColor: "#ffffff",
-          hoverColor: "{zinc.900}",
-          activeColor: "{zinc.800}",
-        },
-        highlight: {
-          background: "{zinc.950}",
-          focusBackground: "{zinc.700}",
-          color: "#ffffff",
-          focusColor: "#ffffff",
-        },
-      },
-      dark: {
-        primary: {
-          color: "{zinc.50}",
-          inverseColor: "{zinc.950}",
-          hoverColor: "{zinc.100}",
-          activeColor: "{zinc.200}",
-        },
-        highlight: {
-          background: "rgba(250, 250, 250, .16)",
-          focusBackground: "rgba(250, 250, 250, .24)",
-          color: "rgba(255,255,255,.87)",
-          focusColor: "rgba(255,255,255,.87)",
-        },
-      },
-    },
-  },
+const pinia = createPinia();
+const app = createApp(App);
+
+app.component("Row", Row);
+app.component("Card", Card);
+app.component("Dialog", Dialog);
+app.component("Button", Button);
+app.component("Column", Column);
+app.component("Divider", Divider);
+app.component("Dropdown", Dropdown);
+app.component("Checkbox ", Checkbox);
+app.component("InputText", InputText);
+app.component("DataTable", DataTable);
+app.component("ColumnGroup", ColumnGroup);
+
+app.use(PrimeVue, {
+  unstyled: true,
+  pt: MyPreset,
 });
 
-const app = createApp(AppRoot);
-const pinia = createPinia();
 app.use(pinia);
 app.use(router);
-
-// prime vue config
-app.use(PrimeVue, {
-  ripple: false,
-  // Default theme configuration
-  theme: {
-    base: PrimeOne,
-    preset: Aura,
-    options: {
-      prefix: "p",
-      darkModeSelector: "system",
-      cssLayer: false,
-    },
-  },
-});
-
-app.component("Card", Card);
-app.component("Button", Button);
-app.component("DataTable", DataTable);
-app.component("Column", Column);
-app.component("ColumnGroup", ColumnGroup);
-app.component("Row", Row);
-app.component("Dropdown", Dropdown);
-app.component("InputText", InputText);
-app.component("Divider", Divider);
-app.component("Checkbox ", Checkbox);
-// prime vue config end
-
 app.mount("#app");
--- src/pages/AboutView.vue
+++ src/pages/AboutView.vue
@@ -1,6 +1,28 @@
 <template>
   <div>
-    Welcome to about us
-    <Button outlined>Edit Columns</Button>
+    <Button class="
+            inline-flex
+            items-center
+            justify-center
+            rounded-md
+            text-sm
+            font-medium
+            ring-offset-background
+            transition-colors transition-duration-200
+            focus-visible:outline-none
+            focus-visible:ring-2
+            focus-visible:ring-ring
+            focus-visible:ring-offset-2
+            disabled:pointer-events-none disabled:opacity-50
+            bg-gray-900
+            text-white
+            dark:bg-white dark:text-black dark:hover:bg-white/90
+            hover:bg-gray-700
+            h-10
+            px-4
+            py-2
+          ">
+      Save changes
+    </Button>
   </div>
 </template>
